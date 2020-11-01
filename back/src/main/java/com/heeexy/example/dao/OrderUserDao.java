d15f56f79ddb532e5aa090925eca9ff4e240ec1f
Ywz2232 <969157231@qq.com>
2020-11-01 17:18:30 +0800

    order修改订单商品数量

---
+++ back/src/main/java/com/heeexy/example/dao/OrderUserDao.java
@@ -0,0 +1,7 @@
+package com.heeexy.example.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heeexy.example.pojo.OrderUser;
+
+public interface OrderUserDao extends BaseMapper<OrderUser> {
+}
